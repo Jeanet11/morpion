7fc5f918fbd4491d7e076d67561bf54be7b264c6
Jeanne <jeanne.tiramani@hotmail.fr>
2017-08-02 18:34:38 +0200

    alternance des joueurs et condition pour vérifier si case occupée

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-console.log('test');
-$(".case").click((function) {
-	console.log("test");
-	//$(this).append(smile.jpg);
+var count = 0;
 
+$(".case").click(function(){
+	// on vérifie que la case n'est pas occupée
+	if($(this).hasClass('vide')) {
+		
+
+		$(this).removeClass('vide').addClass('pleine');
+		count++;
+		//pour alterner les joueurs
+		if(count%2 === 0) {
+
+			$(this).html('<img   src="smile.jpg"/>');
+			
+		} else {
+			$(this).html('<img   src="vache.png"/>');
+		}
+	}
+	else {
+		alert("Case occupée, rejoue !");
+	}
 });
